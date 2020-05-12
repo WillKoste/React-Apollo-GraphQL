ed836b3a0bb806e7215bead353820cf959752ebc
Will Koste <willkoste@gmail.com>
2020-05-12 10:43:31 -0400

    Application Complete

--- client/src/components/Launch.js
+++ client/src/components/Launch.js
@@ -1,11 +1,66 @@
-import React from 'react';
+  
+import React, { Component, Fragment } from 'react';
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
+import { Link } from 'react-router-dom';
+import classNames from 'classnames';
 
-const Launch = () => {
-  return (
-    <div>
-      <h2>Hey :D</h2>
-    </div>
-  )
+const LAUNCH_QUERY = gql`
+  query LaunchQuery($flight_number: Int!) {
+    launch(flight_number: $flight_number) {
+      flight_number
+      mission_name
+      launch_year
+      launch_success
+      launch_date_local
+      rocket {
+        rocket_id
+        rocket_name
+        rocket_type
+      }
+    }
+  }
+`;
+
+export class Launch extends Component {
+  render() {
+    let { flight_number } = this.props.match.params;
+    flight_number = parseInt(flight_number);
+    return (
+      <Fragment>
+        <Query query={LAUNCH_QUERY} variables={{flight_number}}>
+          {
+            ({loading, error, data}) => {
+              if(loading) return <h4>Loading...</h4>
+              if(error) console.error(error);
+              const {mission_name, launch_year, launch_success, rocket: {rocket_id, rocket_name, rocket_type}} = data.launch;
+
+              return <div>
+                <h1 className="display-4 my-3"><span className="text-dark">Mission:</span> {mission_name} </h1>
+                <h3 className="py-3 mt-5 text-primary">Launch Details</h3>
+                <ul className="list-group">
+                  <li className="list-group-item">Flight Number: {flight_number}</li>
+                  <li className="list-group-item">Launch Year: {launch_year}</li>
+                  <li className="list-group-item">Launch Successful: <span className={classNames({
+                    'text-success': launch_success,
+                    'text-danger': !launch_success
+                  })}>{launch_success ? 'True' : 'False'}</span></li>
+                </ul>
+                <h3 className="py-3 mt-5 text-primary">Rocket Details</h3>
+                <ul className="list-group">
+                  <li className="list-group-item">Rocket ID: {rocket_id}</li>
+                  <li className="list-group-item">Rocket Name: {rocket_name}</li>
+                  <li className="list-group-item">Rocket Type: {rocket_type}</li>
+                </ul>
+                <hr/>
+                <Link to="/" className="btn btn-secondary my-3">Go Back</Link>
+              </div>
+            }
+          }
+        </Query>
+      </Fragment>
+    );
+  }
 }
 
 export default Launch;
--- client/src/components/Launches.js
+++ client/src/components/Launches.js
@@ -1,11 +1,11 @@
-import React, {Fragment} from 'react';
+import React, { Component, Fragment } from 'react';
 import gql from 'graphql-tag';
-import {Query} from 'react-apollo';
+import { Query } from 'react-apollo';
 import LaunchItem from './LaunchItem';
 import MissionKey from './MissionKey';
 
 const LAUNCHES_QUERY = gql`
-  query LaunchesQuery{
+  query LaunchesQuery {
     launches {
       flight_number
       mission_name
@@ -15,29 +15,29 @@ const LAUNCHES_QUERY = gql`
   }
 `;
 
-const Launches = () => {
-  return (
-    <Fragment>
-      <h1 className="display-4 my-3">Launches</h1>
-      <MissionKey />
-      <Query query={LAUNCHES_QUERY}>
-        {
-          ({loading, error, data}) => {
-            if(loading) return <h4>Loading...</h4>
-            if(error) console.log(error);
+export class Launches extends Component {
+  render() {
+    return (
+      <Fragment>
+        <h1 className="display-4 my-3">Launches</h1>
+        <MissionKey />
+        <Query query={LAUNCHES_QUERY}>
+          {({ loading, error, data }) => {
+            if (loading) return <h4>Loading...</h4>;
+            if (error) console.log(error);
 
-            return <Fragment>
-              {
-                data.launches.map(launch => (
+            return (
+              <Fragment>
+                {data.launches.map(launch => (
                   <LaunchItem key={launch.flight_number} launch={launch} />
-                ))
-              }
-            </Fragment>
-          }
-        }
-      </Query>
-    </Fragment>
-  )
+                ))}
+              </Fragment>
+            );
+          }}
+        </Query>
+      </Fragment>
+    );
+  }
 }
 
 export default Launches;
