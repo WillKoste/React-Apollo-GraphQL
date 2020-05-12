f6208b906634f303f1509759907e0a210def3981
Will Koste <willkoste@gmail.com>
2020-05-12 10:56:47 -0400

    Deployment

---
+++ public/precache-manifest.bdbb08cd653a47263dc1077daa7cde9d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8d202622d3708b573513ea97d98ac9d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "91ec06f4313d59f3c3a8",
+    "url": "/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "e272b574f33be7446700",
+    "url": "/static/js/2.79a95ac4.chunk.js"
+  },
+  {
+    "revision": "b74364752c6386920aced16a152e4b46",
+    "url": "/static/js/2.79a95ac4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91ec06f4313d59f3c3a8",
+    "url": "/static/js/main.0f8490fb.chunk.js"
+  },
+  {
+    "revision": "0e35831b56fb2c044e12",
+    "url": "/static/js/runtime-main.98a2b520.js"
+  }
+]);
